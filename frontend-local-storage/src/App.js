ecb4b01db66c96145aecf29e6746a734cad6359a
AlanDev <ingescobedoalan4@gmail.com>
2022-03-05 13:26:28 -0600

    Se agregaron rulas relativas dependiendo el entorno de desarrollo

--- frontend-local-storage/src/App.js
+++ frontend-local-storage/src/App.js
@@ -1,43 +1,60 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react'
+import Upload from './components/uploadFiles'
 
 const App = () => {
-    const [ facts, setFacts ] = useState([]);
-    const [ listening, setListening ] = useState(false);
-  
-    useEffect( () => {
-      if (!listening) {
-        const events = new EventSource('http://localhost:3000/events');
-  
-        events.onmessage = (event) => {
-          const parsedData = JSON.parse(event.data);
-  
-          setFacts((facts) => facts.concat(parsedData));
-        };
-  
-        setListening(true);
-      }
-    }, [listening, facts]);
-  
-    return (
-      <table className="stats-table">
-        <thead>
-          <tr>
-            <th>Fact</th>
-            <th>Source</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            facts.map((fact, i) =>
-              <tr key={i}>
-                <td>{fact.info}</td>
-                <td>{fact.source}</td>
-              </tr>
-            )
-          }
-        </tbody>
-      </table>
-    );
-  }
+  const [facts, setFacts] = useState([])
+  const [listening, setListening] = useState(false)
+  const token = localStorage.getItem('token')
+  const user = JSON.parse(token)
 
+  const url = `http://localhost:5000/events/${user}`
+  const buildUrl = `/events${user}`
+
+  const subscribeUrl = process.env.REACT_ENV !== 'production' ? url : buildUrl
+
+  useEffect(() => {
+    if (!listening) {
+      const events = new EventSource(subscribeUrl)
+
+      events.onmessage = (event) => {
+        const data = JSON.parse(event.data)
+        console.log({
+          initialEvent: data,
+          event
+        })
+        const parsedData = JSON.parse(event.data)
+        setFacts((facts) => facts.concat(parsedData))
+      }
+
+      setListening(true)
+    }
+  }, [listening, facts])
+
+  console.log(facts)
+
+  return (
+<div>
+    <div>
+      <Upload user={user} />
+    </div>
+    <table className="stats-table">
+      <thead>
+        <tr>
+          <th>Fact</th>
+          <th>Source</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          facts.map((fact, i) =>
+            <tr key={i}>
+              <td>{fact.name}</td>
+            </tr>
+          )
+        }
+      </tbody>
+    </table>
+  </div>
+  )
+}
 export default App

55c7efc2e65d7a82e72086b2f88fba69e6c91f4a
AlanDev <ingescobedoalan4@gmail.com>
2022-03-06 23:41:58 -0600

    Metamask button redirect to home app

--- frontend-local-storage/src/components/login/MetamaskButton.js
+++ frontend-local-storage/src/components/login/MetamaskButton.js
@@ -1,7 +1,15 @@
+import { useNavigate } from 'react-router-dom'
 
 const MetamaskButton = () => {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate('/home')
+  }
+
   return (
     <button
+    onClick={handleClick}
       type="button"
       className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2"
     >
